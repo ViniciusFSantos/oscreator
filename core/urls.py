0d0dd8664e00f54be61921e4b28d18a4f0e242bd
unknown <viniciusfdossantos@yahoo.com>
2022-02-21 13:16:48 -0300

    iniciado criação do mainboard

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, MainBoard
 from django.conf.urls.static import static
 from django.conf import settings
 
 
 urlpatterns = [
     path('',IndexView.as_view(), name='index'),
+    path('mainboard',MainBoard.as_view(), name='mainboard'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,15 @@
+from urllib import request
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib import auth
 
 class IndexView(TemplateView):
     template_name = 'index.html'
     pass    
+
+
+class MainBoard(TemplateView):
+    template_name = 'mainboard.html'
+    #!!TODO
+    usuario = auth.get_user()
+    pass
