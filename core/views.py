6d2d870df162b07ec446bafb15a1c474210b1b86
unknown <viniciusfdossantos@yahoo.com>
2022-02-22 17:19:03 -0300

    modificado views.py

--- core/views.py
+++ core/views.py
@@ -1,21 +1,17 @@
 from urllib import request
 from django.shortcuts import render
-from django.views.generic import TemplateView, View, ListView
+from django.views.generic import TemplateView, View, ListView, DetailView
 from core.models import Servico
 
 class IndexView(TemplateView):
     template_name = 'index.html'
     pass    
 
-class MainBoard(ListView):
-    model = Servico
+class MainBoard(View):
     template_name = 'mainboard.html'
-    context_object_name = 'servicos'
-    not_auth = 'notauth.html'
-    
-    
-    '''def get(self, request):
+
+    def get(self, request):
         if request.user.is_authenticated:
             return render(request, self.template_name)
         else:
-            return render(request, self.not_auth)'''
+            return render(request, self.not_auth)
