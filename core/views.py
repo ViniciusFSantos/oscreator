841862421de43a365d1a4081767c03241be5ab94
Vinicius Fernandes <viniciusfdossantos@yahoo.com>
2022-02-21 21:16:17 -0300

    Adicionado verificação de usuário para visualizar a mainboard

--- core/views.py
+++ core/views.py
@@ -1,15 +1,17 @@
 from urllib import request
 from django.shortcuts import render
-from django.views.generic import TemplateView
-from django.contrib import auth
+from django.views.generic import TemplateView, View
 
 class IndexView(TemplateView):
     template_name = 'index.html'
     pass    
 
 
-class MainBoard(TemplateView):
+class MainBoard(View):
     template_name = 'mainboard.html'
-    #!!TODO
-    usuario = auth.get_user()
-    pass
+    not_auth = 'notauth.html'
+    def get(self, request):
+        if request.user.is_authenticated:
+            return render(request, self.template_name)
+        else:
+            return render(request, self.not_auth)
