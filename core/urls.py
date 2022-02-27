3a821aa1547a98beedd90e5fb211925dba17ae27
ViniciusFSantos <viniciusfdossantos@yahoo.com>
2022-02-27 19:50:13 -0300

    iniciando update forms

--- core/urls.py
+++ core/urls.py
@@ -1,17 +1,18 @@
 from django.contrib import admin
 from django.urls import path
-from .views import IndexView, MainBoard, ClienteView, ServicoView, OsView, ClienteFormView, ServicoFormView, OsFormView
+from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
 
 urlpatterns = [
-    path('',IndexView.as_view(), name='index'),
-    path('mainboard/', MainBoard.as_view(), name='mainboard'),
-    path('mainboard/clientes', ClienteView.as_view(), name='clientes'),
-    path('mainboard/servicos', ServicoView.as_view(), name='servicos'),
-    path('mainboard/os', OsView.as_view(), name='os'),
-    path('mainboard/clientes/new', ClienteFormView.as_view(), name='clientes_new'),
-    path('mainboard/servicos/new', ServicoFormView.as_view(), name='servicos_new'),
-    path('mainboard/os/new', OsFormView.as_view(), name='os_new'),
+    path('',views.IndexView.as_view(), name='index'),
+    path('mainboard/', views.MainBoard.as_view(), name='mainboard'),
+    path('mainboard/clientes', views.ClienteView.as_view(), name='clientes'),
+    path('mainboard/servicos', views.ServicoView.as_view(), name='servicos'),
+    path('mainboard/os', views.OsView.as_view(), name='os'),
+    path('mainboard/clientes/new', views.ClienteFormView.as_view(), name='clientes_new'),
+    path('mainboard/servicos/new', views.ServicoFormView.as_view(), name='servicos_new'),
+    path('mainboard/os/new', views.OsFormView.as_view(), name='os_new'),
+    path('mainboard/servicos/<pk>', views.ServicoUpdateView.as_view(), name='servicos_update'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,49 +1,100 @@
-from django.views import generic
+from django.http import HttpResponseRedirect
+from django.views.generic import TemplateView, ListView
 from core.models import Servico, Cliente, Os
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic.edit import FormView
 from .forms import ClienteForm, ServicoForm, OsForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import FormView, UpdateView
 
-class IndexView(generic.TemplateView):
+# ----- Views -----
+class IndexView(TemplateView):
     template_name = 'index.html'
     pass    
 
-class MainBoard(LoginRequiredMixin, generic.TemplateView):
+class MainBoard(LoginRequiredMixin, TemplateView):
     template_name = 'mainboard.html'
-    pass
 
-class ClienteView(LoginRequiredMixin, generic.ListView):
+
+class ClienteView(LoginRequiredMixin, ListView):
     template_name = 'cliente.html'
     context_object_name = 'clientes'
     def get_queryset(self):
         return Cliente.objects.all()
 
-class ServicoView(LoginRequiredMixin, generic.ListView):
+class ServicoView(LoginRequiredMixin, ListView):
     template_name = 'servico.html'
     context_object_name = 'servicos'
     def get_queryset(self):
         return Servico.objects.all()
 
-class OsView(LoginRequiredMixin, generic.ListView):
+class OsView(LoginRequiredMixin, ListView):
     template_name = 'os.html'
     context_object_name = 'OSs'
     def get_queryset(self):
         return Os.objects.all()
-
-class ClienteFormView(FormView):
+# ---------------------
+# ------- Create ------
+class ClienteFormView(LoginRequiredMixin, FormView):
     template_name = 'novocliente.html'
     form_class = ClienteForm
     def form_valid(self, form):
-        return super().form_valid(form)
+        form.save()        
+        return HttpResponseRedirect('/mainboard/clientes')
 
-class ServicoFormView(FormView):
+class ServicoFormView(LoginRequiredMixin, FormView):
     template_name = 'novoservico.html'
     form_class = ServicoForm
     def form_valid(self, form):
-        return super().form_valid(form)
+        form.save()        
+        return HttpResponseRedirect('/mainboard/servicos')
 
 class OsFormView(FormView):
     template_name = 'novaos.html'
     form_class = OsForm
     def form_valid(self, form):
-        return super().form_valid(form)
+        form.save()        
+        return HttpResponseRedirect('/mainboard/os')
+# ---------------------
+# ------- Update ------
+class ClienteUpdateView(LoginRequiredMixin, UpdateView):
+    model = Cliente
+    template_name = 'clienteupdate.html'
+    fields = [
+        'nome',
+        'cpf_cnpj',
+        'rg_ie',
+        'telefone1',
+        'telefone2',
+        'email',
+        'endereco',
+        'bairro',
+        'cidade',
+        'uf',
+    ]
+    success_url = '/mainboard/clientes'
+
+class ServicoUpdateView(LoginRequiredMixin, UpdateView):
+    model = Servico
+    template_name = 'servicoupdate.html'
+    fields = [
+        'servico', 
+        'descricao',
+    ]
+    success_url = '/mainboard/servicos'
+
+
+class OsUpdateView(LoginRequiredMixin, UpdateView):
+    model = Servico
+    template_name = 'osupdate.html'
+    fields = [
+        'cliente',
+        'relato',
+        'defeito_encontrado',
+        'servico',
+        'desc_serv',
+        'garantia',
+        'serv_valor',
+        'pec_valor',
+    ]
+    success_url = '/mainboard/os'
+# ---------------------
+# ------ Delete ------
