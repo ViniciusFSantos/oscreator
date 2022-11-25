979c41be911fca327ed0af38de67b76baa7aedb5
Vinícius Fernandes dos Santos <62731699+ViniciusFSantos@users.noreply.github.com>
2022-11-24 22:38:37 -0300

    Update settings.py

--- oscreator/settings.py
+++ oscreator/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'oscreator',
         'USER': 'postgres',
-        'PASSWORD': 'flamengo5240',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
         
